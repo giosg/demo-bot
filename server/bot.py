44487482fbf85b581acb39cc1d701777af9dbc84
Kimmo Kiiski <kimmo@giosg.com>
2018-09-18 16:09:13 +0300

    Converted API helper module to a class

--- server/bot.py
+++ server/bot.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from conf import INVITEE_TEAM_ID
-import giosg
+from giosg import APIClient
 
 
 class ChatBot(object):
@@ -9,43 +9,37 @@ class ChatBot(object):
     sending new chat message or updating presence in giosg system.
     """
     def __init__(self, auth):
+        self.api = APIClient(auth)
         self.auth = auth
 
     def update_or_create_user_client(self):
         # Get existing user client if one
-        user_clients = giosg.list(
-            url='/api/v5/users/{user_id}/clients'.format(**self.auth),
-            auth=self.auth,
-        )
+        user_clients = self.api.list('/api/v5/users/{user_id}/clients'.format(**self.auth))
         if user_clients:
             client_id = user_clients[0]['id']
-            giosg.update(
+            self.api.update(
                 url='/api/v5/users/{user_id}/clients/{client_id}'.format(client_id=client_id, **self.auth),
-                auth=self.auth,
                 payload={
                     'presence_expires_in': 60,
                 },
             )
         else:
-            giosg.create(
+            self.api.create(
                 url='/api/v5/users/{user_id}/clients'.format(**self.auth),
-                auth=self.auth,
                 payload={
                     'presence_expires_in': 60,
                 },
             )
 
     def is_allowed_to_join(self, chat_id):
-        chat = giosg.retrieve(
+        chat = self.api.retrieve(
             url='/api/v5/users/{user_id}/routed_chats/{chat_id}'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
         )
         return chat['present_user_participant_count'] == 0
 
     def join_to_chat(self, chat_id):
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/routed_chats/{chat_id}/memberships'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 'is_participating': True,
                 'composing_status': 'idle',
@@ -56,9 +50,8 @@ class ChatBot(object):
         """
         Format welcoming message and makes API request
         """
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/messages'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "message": "Welcome to giosg's support side,",
                 "attachments": [{
@@ -93,9 +86,8 @@ class ChatBot(object):
         )
 
     def react_to_customer_service_agent(self, chat_id):
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/messages'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "message": "Here's some information for customer service agents!",
                 "attachments": [{
@@ -129,9 +121,8 @@ class ChatBot(object):
         )
 
     def react_to_manager_user(self, chat_id):
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/messages'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "message": "Here's some information that is helpful for manager users!",
                 "attachments": [{
@@ -165,9 +156,8 @@ class ChatBot(object):
         )
 
     def react_to_developer(self, chat_id):
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/messages'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "message": "Oh, you are a developer! I'm also been created by a developer! Here's some nerdy information for you!",
                 "attachments": [{
@@ -201,23 +191,20 @@ class ChatBot(object):
         )
 
     def react_to_request_human(self, chat_id):
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/outgoing_chat_invitations'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "invited_team_id": INVITEE_TEAM_ID,
             },
         )
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/messages'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "message": "OK, I invited my fellow human co-worker to this chat! They will join in a moment and I'll leave you guys!",
             },
         )
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/messages'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "message": "But before I go, could you tell me if you found this information helpful?",
                 "attachments": [{
@@ -241,9 +228,8 @@ class ChatBot(object):
         )
 
     def react_to_positive_feedback(self, chat_id):
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/messages'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "message": "Good to hear that! My human fellow will continue chatting with you!",
             },
@@ -251,9 +237,8 @@ class ChatBot(object):
         self.leave_chat_conversation(chat_id)
 
     def react_to_negative_feedback(self, chat_id):
-        giosg.create(
+        self.api.create(
             url='/api/v5/users/{user_id}/chats/{chat_id}/messages'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "message": "Sorry to hear that! I hope that my human fellow can serve you better!",
             },
@@ -264,9 +249,8 @@ class ChatBot(object):
         pass
 
     def leave_chat_conversation(self, chat_id):
-        giosg.update(
+        self.api.update(
             url='/api/v5/users/{user_id}/chat_memberships/{chat_id}'.format(chat_id=chat_id, **self.auth),
-            auth=self.auth,
             payload={
                 "is_participating": False,
             },
--- server/giosg.py
+++ server/giosg.py
@@ -2,46 +2,47 @@ from conf import SERVICE_URL
 from retry_session import retry_session
 
 
-def list(url, auth):
-    results = []
-    while url:
-        page = retrieve(url, auth)
-        results.extend(page['results'])
-        url = page['next']
-    return results
+class APIClient:
 
+    def __init__(self, auth):
+        self.auth = auth
 
-def retrieve(url, auth):
-    session = retry_session(2)
-    response = session.get(SERVICE_URL + url, headers={
-        'Authorization': '{token_type} {access_token}'.format(**auth),
-    })
-    response.raise_for_status()
-    return response.json()
+    def list(self, url):
+        results = []
+        while url:
+            page = self.retrieve(url)
+            results.extend(page['results'])
+            url = page['next']
+        return results
 
+    def retrieve(self, url):
+        session = retry_session(2)
+        response = session.get(SERVICE_URL + url, headers={
+            'Authorization': '{token_type} {access_token}'.format(**self.auth),
+        })
+        response.raise_for_status()
+        return response.json()
 
-def create(url, payload, auth):
-    session = retry_session(2)
-    response = session.post(SERVICE_URL + url, json=payload, headers={
-        'Authorization': '{token_type} {access_token}'.format(**auth),
-    })
-    response.raise_for_status()
-    return response.json()
+    def create(self, url, payload):
+        session = retry_session(2)
+        response = session.post(SERVICE_URL + url, json=payload, headers={
+            'Authorization': '{token_type} {access_token}'.format(**self.auth),
+        })
+        response.raise_for_status()
+        return response.json()
 
+    def update(self, url, payload):
+        session = retry_session(2)
+        response = session.patch(SERVICE_URL + url, json=payload, headers={
+            'Authorization': '{token_type} {access_token}'.format(**self.auth),
+        })
+        response.raise_for_status()
+        return response.json()
 
-def update(url, payload, auth):
-    session = retry_session(2)
-    response = session.patch(SERVICE_URL + url, json=payload, headers={
-        'Authorization': '{token_type} {access_token}'.format(**auth),
-    })
-    response.raise_for_status()
-    return response.json()
-
-
-def destroy(url, auth):
-    session = retry_session(2)
-    response = session.delete(SERVICE_URL + url, headers={
-        'Authorization': '{token_type} {access_token}'.format(**auth),
-    })
-    response.raise_for_status()
-    return response.json()
+    def destroy(self, url):
+        session = retry_session(2)
+        response = session.delete(SERVICE_URL + url, headers={
+            'Authorization': '{token_type} {access_token}'.format(**self.auth),
+        })
+        response.raise_for_status()
+        return response.json()
