c3ba279d9f108473c11963589eeaccda15a13240
Kimmo Kiiski <kimmo@giosg.com>
2018-09-18 13:25:24 +0300

    Service URL environment variable is optional

--- server/conf.py
+++ server/conf.py
@@ -4,5 +4,5 @@ Collect the app settings from the environment variables as Python constants.
 import os
 
 INVITEE_TEAM_ID = os.environ["INVITEE_TEAM_ID"]
-SERVICE_URL = os.environ["SERVICE_URL"]
+SERVICE_URL = os.environ.get("SERVICE_URL") or 'https://service.giosg.com'
 SECRET_STRING = os.environ["SECRET_STRING"]
