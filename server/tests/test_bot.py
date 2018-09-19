274aa7b47e3e16e3a78ca23f9b8c2f5de9add6bc
Kimmo Kiiski <kimmo@giosg.com>
2018-09-19 14:01:50 +0300

    Test bot reaction to visitor chat message

--- server/tests/test_bot.py
+++ server/tests/test_bot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from server.bot import ChatBot
 import responses
@@ -96,3 +97,61 @@ class BotTest(unittest.TestCase):
         self.assertEqual(req3.request.url, 'https://service.giosg.com/api/v5/users/user1/routed_chats/chat1/memberships')
         self.assertEqual(json.loads(req3.request.body), {"is_participating": True, "composing_status": "idle"})
         self.assertEqual(req4.request.url, 'https://service.giosg.com/api/v5/users/user1/chats/chat1/messages')
+
+    @responses.activate
+    def test_handle_new_visitor_chat_message(self):
+        responses.add(responses.GET, 'https://service.giosg.com/api/v5/users/user1/clients', json={'results': [], 'next': None})
+        responses.add(responses.POST, 'https://service.giosg.com/api/v5/users/user1/clients')
+        responses.add(responses.POST, 'https://service.giosg.com/api/v5/users/user1/routed_chats/chat1/memberships')
+        responses.add(responses.POST, 'https://service.giosg.com/api/v5/users/user1/chats/chat1/messages')
+        self.bot.handle_new_user_chat_message({
+            'id': 'message1',
+            'chat_id': 'chat1',
+            'type': 'msg',
+            'sender_type': 'visitor',
+            'response_value': None,
+        })
+        req1, req2, req3 = responses.calls
+        self.assertEqual(req1.request.url, 'https://service.giosg.com/api/v5/users/user1/clients')
+        self.assertEqual(req2.request.url, 'https://service.giosg.com/api/v5/users/user1/clients')
+        self.assertEqual(req2.request.method, 'POST')
+        self.assertEqual(json.loads(req2.request.body), {"presence_expires_in": 7200})
+        self.assertEqual(req3.request.url, 'https://service.giosg.com/api/v5/users/user1/chats/chat1/messages')
+        self.assertEqual(json.loads(req3.request.body), {
+            "message": "I apologize, I'm just a simple example bot uncapable of understanding human language! 😅",
+            "attachments": [{
+                "text": "I only know what to do if you choose one of the options below!",
+                "actions": [{
+                    "text": "Customer service agent",
+                    "type": "link_button",
+                    "link_target": "_parent",
+                    "value": "https://www.giosg.com/support/user",
+                    "style": "brand_primary",
+                    "is_disabled_on_selection": True,
+                    "is_disabled_on_visitor_message": True
+                }, {
+                    "text": "Manager user",
+                    "type": "link_button",
+                    "link_target": "_parent",
+                    "value": "https://www.giosg.com/support/manager",
+                    "style": "brand_primary",
+                    "is_disabled_on_selection": True,
+                    "is_disabled_on_visitor_message": True
+                }, {
+                    "text": "Developer",
+                    "type": "link_button",
+                    "link_target": "_parent",
+                    "value": "https://www.giosg.com/support/developer",
+                    "style": "brand_primary",
+                    "is_disabled_on_selection": True,
+                    "is_disabled_on_visitor_message": True
+                }, {
+                    "text": "Let me chat with a human",
+                    "type": "button",
+                    "value": "request_human",
+                    "style": "brand_secondary",
+                    "is_disabled_on_selection": True,
+                    "is_disabled_on_visitor_message": True
+                }]
+            }],
+        })
